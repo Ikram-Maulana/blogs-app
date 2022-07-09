a5e906e1c98f4e3ea0d855ad42ff49daa443d431
ikram-maulana <ikram075@ummi.ac.id>
2022-07-09 14:30:21 +0700

    Memberikan Style Pada Halaman 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,17 @@
+import { Card } from "flowbite-react";
 import React from "react";
 
 const NotFound = () => {
   return (
     <section className="p-5">
-      <h1>404</h1>
-      <p>Request Not found</p>
+      <Card>
+        <h5 className="text-2xl text-center md:text-left font-bold tracking-tight text-gray-900 dark:text-white">
+          404
+        </h5>
+        <p className="font-normal text-center md:text-left text-gray-700 dark:text-gray-400">
+          Request Not Found
+        </p>
+      </Card>
     </section>
   );
 };
